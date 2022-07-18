3cfb8fca70bbe0ecb7e1506e63db5d940efb1d7f
YadiBelly <Yadisabell96@gmail.com>
2022-07-18 23:43:48 +0000

    merge

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -6,7 +6,6 @@ import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
-import { App } from "./pages/calendar.js";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
@@ -23,7 +22,7 @@ const Layout = () => {
           <Navbar />
           <Routes>
             <Route element={<Home />} path="/" />
-            <Route element={<App />} path="/app" />
+
             <Route element={<Demo />} path="/demo" />
             <Route element={<Single />} path="/single/:theid" />
             <Route element={<h1>Not found!</h1>} />
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -11,7 +11,6 @@ export const Home = () => {
   const { store, actions } = useContext(Context);
 
   return (
-
     <div className="text-center mt-5">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
@@ -34,36 +33,35 @@ export const Home = () => {
       </div>
       <h1 class="uppercase">VEHICLE MAINTANCE TRACKER</h1>
       <p></p>
-       <div>
-    <p className="uppercase mt-5">
-        <img src={vmt} />
-      </p>
+      <div>
+        <p className="uppercase mt-5">
+          <img src={vmt} />
+        </p>
 
-    <div className="belle">
-      <AiOutlineBell className="bell" alt="..." />
-      <p className="p1">
-        <strong>
-          This application is to help individuals with alerts on a vehicle with
-          any necessary maintenance. Individuals , who are not knowledgeable on
-          vehicles or sufficient with updated maintenance can now keep track of
-          the last and next maintenance needed along with obtaining details on
-          why a particular service is needed.
-        </strong>
-      </p>
-    </div>
-    <div className="quee">
-    <BsQuestionLg className="que" />
-      <p className="p2">
-      <strong>
-                    This application is different from others because it allows
-                    users to stay on schedule with auto services. This app also
-                    lets the user know the reason for the service and how it can
-                    benefit the vehicle.
-                  </strong>
-      </p>
-      
-    </div>
-
+        <div className="belle">
+          <AiOutlineBell className="bell" alt="..." />
+          <p className="p1">
+            <strong>
+              This application is to help individuals with alerts on a vehicle
+              with any necessary maintenance. Individuals , who are not
+              knowledgeable on vehicles or sufficient with updated maintenance
+              can now keep track of the last and next maintenance needed along
+              with obtaining details on why a particular service is needed.
+            </strong>
+          </p>
+        </div>
+        <div className="quee">
+          <BsQuestionLg className="que" />
+          <p className="p2">
+            <strong>
+              This application is different from others because it allows users
+              to stay on schedule with auto services. This app also lets the
+              user know the reason for the service and how it can benefit the
+              vehicle.
+            </strong>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
